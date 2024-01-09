cf370b3163965e388520f5d48486c45c9e37f787
theZalmanian <syntaxwarden@protonmail.com>
2024-01-09 13:15:12 -0500

    Added process.php to process orders

---
+++ process.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ice Cream Order Summary</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>
+            Thank you for you order!
+        </h1>
+    </div>
+    </body>
+</html>
